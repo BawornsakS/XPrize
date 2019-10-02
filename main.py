a838ff49aafcfdaffe124f29653316e2295d6f8e
BuildTools <unconfigured@null.spigotmc.org>
2019-10-02 10:38:32 +0700

    Julong commits

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-print ("Welcome to FIBO XPrize Software Team!")
-print ("Blink is on the team. <3")
+try:
+    print ("Welcome to FIBO XPrize Software Team!")
+    print ("Julong is on the team. <3")
+    print ("Julong is on the team. <3")
+    
+except:
+    print("Welcome to FIBO XPrize Software Team!")
+    print("Julong is on the team. <3")
