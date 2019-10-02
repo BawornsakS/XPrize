def54ea95d8f9ad256f9b68ea8fe017d6dc24260
EpicFIBO <42671786+EpicFibo@users.noreply.github.com>
2019-10-02 10:49:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-try:
-    print ("Welcome to FIBO XPrize Software Team!")
-    print ("Blink is on the team. <3")
-    print ("Julong is on the team. <3")
-    
-except:
-    print("Welcome to FIBO XPrize Software Team!")
-    print("Julong is on the team. <3")
+print ("Welcome to FIBO XPrize Software Team!")
+print ("Blink is on the team. <3")
+print ("Julong is on the team. <3")
