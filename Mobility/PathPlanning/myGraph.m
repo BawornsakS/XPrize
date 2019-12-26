862b4aa766d1afb31e2d8d7c684bbdadcb29b7fa
Sornpraram <36271349+Sornpraram@users.noreply.github.com>
2019-12-26 16:45:11 +0700

    add Help

--- Mobility/PathPlanning/myGraph.m
+++ Mobility/PathPlanning/myGraph.m
@@ -1,5 +1,12 @@
 function g_out = myGraph(bw,x_cm,per)
 
+% myGraph  function have 3 Argument
+%   C = myGraph(pgm_file,x-axis_length,x-axis_pixel_length) adds A to itself.
+%   
+%   pgm_file : will accept any portable gray map file (.pgm)
+%   x-axis_length : you need to determine your own map x-axis length (centimeter)
+%   x-axis_pixel_length : determine the scale of each pixel (centimeter)
+%   
 I=imread(bw);
 [H L]=size(I);
 
@@ -20,4 +27,5 @@ gedge = gg.Edges;
 writetable(gnode,'myNodes.txt','Delimiter',' '); %Write down Node in Txt File
 writetable(gedge,'myEdges.txt','Delimiter',' '); %Write down Edge in Txt File
 dlmwrite('myAdjacent.txt',[col row v], 'delimiter', '\t') %Write down Adjacent Sparse in Txt File
-writematrix(adjM,'myAdjacentMatrix.txt','Delimiter',' '); %Write down Adjacent Matrix in Txt File
+writematrix(adjM,'myAdjacentMatrix.txt','Delimiter',' '); %Write down Adjacent Matrix in Txt 
+num_nodes = numnodes(gg);%num of node avaliable
